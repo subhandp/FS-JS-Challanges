679d9eebcd1aaf13ef536547b82198828cec6e19
subhandp <subhan.dinda.putra@gmail.com>
2020-07-22 08:04:41 +0800

    perbaiki output console

--- 008.js
+++ 008.js
@@ -1,9 +1,11 @@
 class Str {
     static lower(str) {
+        console.log(str.toLowerCase());
         return str.toLowerCase();
     }
 
     static upper(str) {
+        console.log(str.toUpperCase());
         return str.toUpperCase();
     }
 
@@ -14,12 +16,14 @@ class Str {
             return this.upper(s.substring(0, 1)) + s.substring(1);
         }.bind(this));
 
+        console.log(kapitals.join(' '));
         return kapitals.join(' ');
     }
 
     static reverse(str) {
         //menggunakan split str menjadi array kemudia membalik isi array dengan reverse selanjutnya gabungkan menjadi string dengan join
         const revStr = str.split(' ').reverse().join(' ');
+        console.log(revStr);
         return revStr;
     }
 
@@ -27,11 +31,13 @@ class Str {
         if (Array.isArray(listContain)) {
             for (const item of listContain) {
                 if (str.includes(item)) {
+                    console.log(true);
                     return true;
                 }
             }
             return false;
         } else if (typeof str == 'string') {
+            console.log(str.includes(listContain));
             return str.includes(listContain);
         }
 
@@ -54,6 +60,7 @@ class Str {
             } //akhir for
         } // akhir elseif
 
+        console.log(str);
         return str;
 
     }
@@ -66,7 +73,9 @@ class Str {
 
         str = str.trim(); //hilangkan  whitespace
         str = this.lower(str); //ubah dulu ke huruf kecil semua dengan method properti lower
-        return str.replace(/[^\w ]+/g, '').replace(/ +/g, myCharSlug); //ubah nonaplhanumeric menjadi slug menggunakan regex \W world
+        str = str.replace(/[^\w ]+/g, '').replace(/ +/g, myCharSlug); //ubah nonaplhanumeric menjadi slug menggunakan regex \W world
+        console.log(str);
+        return str;
     }
 
     static count(str) {
@@ -120,41 +129,48 @@ class Str {
     }
 }
 
-// let mystr = new Str();
 
-// let hasil = mystr.capitalize('saya ingin makan');
-console.log(Str.contains('Saya ingin makan sate', 'makan')); // true
-console.log(Str.contains('Saya ingin makan sate', 'rujak')); // false
-console.log(Str.contains('Saya ingin makan sate', ['sate', 'rujak'])); // true
 
-console.log(Str.capitalize('saya ingin makan'));
+Str.lower('MAKAN') // makan
 
-console.log(Str.random()); // hef2nCi273c8D2dz
-console.log(Str.random(6)); // ckS3jP
-console.log(Str.random(32)); // tbFGeCycTBy8FTfXqOTkDd0YtlQngLt4
+Str.upper('malam') // MALAM
+
+Str.capitalize('saya ingin makan') // Saya Ingin Makan
+
+Str.reverse('kasur') // rusak
+
+Str.contains('Saya ingin makan sate', 'makan') // true
+Str.contains('Saya ingin makan sate', 'rujak') // false
+Str.contains('Saya ingin makan sate', ['sate', 'rujak']) // true
+
+Str.random() // hef2nCi273c8D2dz
+Str.random(6) // ckS3jP
+Str.random(32) // tbFGeCycTBy8FTfXqOTkDd0YtlQngLt4
+
 const title = 'JavaScript, TypeScript & Dart - Bahasa mana yang akan populer di 2018?'
-console.log(Str.slug(title, '*'));
-// console.log(Str.slug(title, '_'));
-Str.count('lorem ipsum');
-Str.countWords('lorem ipsum');
+Str.slug(title) // javascript-typescript-dart-bahasa-mana-yang-akan-populer-di-2018
+Str.slug(title, '_') // javascript_typescript_dart_bahasa_mana_yang_akan_populer_di_2018
 
-// const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+Str.count('lorem ipsum') // 11
 
-// Str.trim('Less than 100 characters') // Less than 100 characters
+Str.countWords('lorem ipsum') // 2
 
-// Str.trim(text) // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
+const text1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-// Str.trim(text, 20) // Lorem ipsum dolor si...
+Str.trim('Less than 100 characters')
 
-// Str.trim(text, 20, '·····') // Lorem ipsum dolor si·····
+Str.trim(text1)
 
+Str.trim(text1, 20)
+
+Str.trim(text1, 20, '·····')
 
 const text2 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-Str.trimWords('Less than 30 words') // Less than 30 words
+Str.trimWords('Less than 30 words')
 
-Str.trimWords(text2) // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi...
+Str.trimWords(text2)
 
-Str.trimWords(text2, 3) // Lorem ipsum dolor...
+Str.trimWords(text2, 3)
 
-Str.trimWords(text2, 3, '·····') // Lorem ipsum dolor·····
+Str.trimWords(text2, 3, '·····')
